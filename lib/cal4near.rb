ca8f1bdf8da45a42574339b22f7551868458711a
nakamaksk <adebayanen@gmail.com>
2022-02-11 23:56:47 +0900

    first commit

---
+++ lib/cal4near.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cal4near/version"
+
+module Cal4near
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/cal4near_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Cal4near do
+  it "has a version number" do
+    expect(Cal4near::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
