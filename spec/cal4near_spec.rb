cbb474e51e87250185f24512784b7927a1fb4528
nakamaksk <adebayanen@gmail.com>
2022-02-16 23:27:44 +0900

    テストコード追加

--- spec/cal4near_spec.rb
+++ spec/cal4near_spec.rb
@@ -5,7 +5,9 @@ RSpec.describe Cal4near do
     expect(Cal4near::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context "list" do
+    it "list" do
+      expect(Cal4near.list.class).to eq Hash
+    end
   end
 end
