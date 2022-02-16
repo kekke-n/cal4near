11a3ab634e6d2ecfe9f36c248d22ffc872f299ce
nakamaksk <adebayanen@gmail.com>
2022-02-17 00:17:44 +0900

    テストコード追加

--- spec/cal4near_spec.rb
+++ spec/cal4near_spec.rb
@@ -5,23 +5,69 @@ RSpec.describe Cal4near do
     expect(Cal4near::VERSION).not_to be nil
   end
 
-  context "list" do
-    it "list" do
-      allow(Cal4near).to receive(:events).and_return([google_event_mock])
-      expect(Cal4near.list.class).to eq Hash
+  context ".events" do
+    # 2022 1/1 10:00 - 11:00
+    let(:event_params_1) {
+      {
+        start: google_event_date_time(DateTime.new(2022, 1, 1, 10, 00, 00)),
+        end:   google_event_date_time(DateTime.new(2022, 1, 1, 11, 00, 00))
+      }
+    }
+    # 2022 1/2 13:00 - 14:00
+    let(:event_params_2) {
+      {
+        start: google_event_date_time(DateTime.new(2022, 1, 2, 13, 00, 00)),
+        end:   google_event_date_time(DateTime.new(2022, 1, 2, 14, 00, 00))
+      }
+    }
+    let(:moc_objs) {
+      [
+        google_event(**event_params_1),
+        google_event(**event_params_2),
+      ]
+    }
+
+    before do
+      allow(Cal4near).to receive(:events).and_return(moc_objs)
+    end
+
+    it "return correct count objects" do
+      expect(Cal4near.events.length).to eq moc_objs.length
+    end
+
+    context 'first event' do
+      let(:subject_event) { Cal4near.events[0] }
+
+      it "start.date_time is correct" do
+        expect(subject_event.start.date_time).to eq DateTime.new(2022, 1, 1, 10, 00, 00)
+      end
+
+      it "start.end_time is correct" do
+        expect(subject_event.end.date_time).to eq DateTime.new(2022, 1, 1, 11, 00, 00)
+      end
+    end
+
+    context 'second event' do
+      let(:subject_event) { Cal4near.events[1] }
+
+      it "start.date_time is correct" do
+        expect(subject_event.start.date_time).to eq DateTime.new(2022, 1, 2, 13, 00, 00)
+      end
+
+      it "start.end_time is correct" do
+        expect(subject_event.end.date_time).to eq DateTime.new(2022, 1, 2, 14, 00, 00)
+      end
     end
   end
 
-  def google_event_mock
-    Google::Apis::CalendarV3::Event.new(
-      start: Google::Apis::CalendarV3::EventDateTime.new(
-        date_time: DateTime.now,
-        time_zone: "Asia/Tokyo"
-      ),
-      end: Google::Apis::CalendarV3::EventDateTime.new(
-        date_time: DateTime.now,
+  def google_event(**params)
+    Google::Apis::CalendarV3::Event.new(**params)
+  end
+
+  def google_event_date_time(date_time=DateTime.now)
+      Google::Apis::CalendarV3::EventDateTime.new(
+        date_time: date_time,
         time_zone: "Asia/Tokyo"
       )
-    )
   end
 end
